6b93115041fc8c1fda44337c19ccc1a753365f72
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-05-03 15:05:40 +0200

    Defines the object prototype with  toString() added to it

--- hats_and_things/assets/javascripts/products.js
+++ hats_and_things/assets/javascripts/products.js
@@ -10,4 +10,19 @@ let hat = {
     }
 };
 
-console.log(hat.toString()); // test the function
+// console.log(hat.toString()); // test the function
+
+// Object Constructor function
+
+function Hat(name, price, color, imageHref) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+    this.imageHref = imageHref;
+}
+
+Hat.prototype.toString = function() {
+    return "name: " + this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
+};
+
+let hatObj = new Hat();

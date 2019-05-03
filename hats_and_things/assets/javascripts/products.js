ce99372411f27946812107713a95f181bee151d9
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-05-03 21:31:28 +0200

    Defines Array of all of the hats in the static HTML page

--- hats_and_things/assets/javascripts/products.js
+++ hats_and_things/assets/javascripts/products.js
@@ -25,4 +25,27 @@ Hat.prototype.toString = function() {
     return "name: " + this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
 };
 
-let hatObj = new Hat();
+const hats = []; // The global variable that store the hats array.
+// Baseball caps
+let capRed = new Hat("Baseball Cap", 11.99, "red", "./assets/images/red/hats/1.png");
+let capBlue = new Hat("Baseball Cap", 11.99, "blue", "./assets/images/blue/hats/1.png");
+let capYellow = new Hat("Baseball Cap", 11.99, "yellow", "./assets/images/yellow/hats/1.png");
+let capGreen = new Hat("Baseball Cap", 11.99, "green", "./assets/images/green/hats/1.png");
+
+// Beanies
+let beanieR = new Hat("Beanie", 17.99, "red", "./assets/images/red/hats/2.png");
+let beanieB = new Hat("Beanie", 17.99, "blue", "./assets/images/blue/hats/2.png");
+let beanieG = new Hat("Beanie", 17.99, "green", "./assets/images/green/hats/2.png");
+
+// Straws
+let strawY = new Hat("Straw Hat", 10.99, "yellow", "./assets/images/yellow/hats/3.png");
+let strawB = new Hat("Straw Hat", 10.99, "blue", "./assets/images/blue/hats/3.png");
+
+// Trilbies
+let trilbyR = new Hat("Trilby", 10.99, "red", "./assets/images/red/hats/4.png");
+let trilbyB = new Hat("Trilby", 10.99, "blue", "./assets/images/blue/hats/4.png");
+let trilbyY = new Hat("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.png");
+
+hats.push(capRed, capBlue, capYellow, capGreen, beanieR, beanieB, beanieG, strawY, strawB, trilbyR, trilbyB, trilbyY);
+
+console.log(hats); // Logs (12) [Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat]

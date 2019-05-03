1644b1773d6ae1a8cc21581d23efc2fbfcf4a365
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-05-03 14:49:42 +0200

    Creates a simple object with toString function attached

---
+++ hats_and_things/assets/javascripts/products.js
@@ -0,0 +1,11 @@
+// Creates Hat object
+
+let hat = {
+    name: "name",
+    price: "price",
+    color: "color",
+    imageHref: "imageHref",
+    toString: function() {
+        return this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
+    }
+};

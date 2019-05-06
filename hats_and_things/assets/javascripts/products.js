01fcfd6a7da7f785e0696529fce69fa8de2d77c0
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-05-06 12:08:00 +0200

    Creates the displayHat(hat)function

--- hats_and_things/assets/javascripts/products.js
+++ hats_and_things/assets/javascripts/products.js
@@ -1,13 +1,13 @@
 // Creates Hat object
 
 let hat = {
-    name: "name",
-    price: "price",
-    color: "color",
-    imageHref: "imageHref",
-    toString: function() {
-        return this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
-    }
+  name: "name",
+  price: "price",
+  color: "color",
+  imageHref: "imageHref",
+  toString: function() {
+    return this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
+  }
 };
 
 // console.log(hat.toString()); // test the function
@@ -15,14 +15,14 @@ let hat = {
 // Object Constructor function
 
 function Hat(name, price, color, imageHref) {
-    this.name = name;
-    this.price = price;
-    this.color = color;
-    this.imageHref = imageHref;
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.imageHref = imageHref;
 }
 
 Hat.prototype.toString = function() {
-    return "name: " + this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
+  return "name: " + this.name + ", price: " + this.price + " , color: " + this.color + " , image: " + this.imageHref;
 };
 
 const hats = []; // The global variable that store the hats array.
@@ -48,4 +48,64 @@ let trilbyY = new Hat("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.
 
 hats.push(capRed, capBlue, capYellow, capGreen, beanieR, beanieB, beanieG, strawY, strawB, trilbyR, trilbyB, trilbyY);
 
-console.log(hats); // Logs (12) [Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat, Hat]
+// Defines the displayHat(hat) function that create a Hat object
+function displayHat(hats) {
+  // creates the 1st <div> with class="accessory col-sm-4"
+  let parentDiv = document.querySelector('#products'); // (this is the container)
+  let newDiv = document.createElement('div');
+  newDiv.className = 'accessory col-sm-4';
+  parentDiv.appendChild(newDiv);
+  console.log(newDiv);
+  // creates the 2nd <div> with class="card my-3" (nested within the 1st <div>)
+  let div = document.getElementsByClassName('accessory col-sm-4');
+  let newDiv2 = document.createElement('div');
+  newDiv2.className = 'card my-3';
+  newDiv.appendChild(newDiv2);
+
+  // creates the 3rd <div> with class="currency btn btn-light disabled" (nested within the 2nd <div>)
+  div = document.getElementsByClassName('card my-3');
+  let newDiv3 = document.createElement('div');
+  newDiv3.className = 'currency btn btn-light disabled';
+  newDiv3.textContent = hats.price;
+  newDiv2.appendChild(newDiv3);
+
+  // creates the <img> (nested also within the 2nd <div>)
+  div = document.getElementsByClassName('currency btn btn-light disabled');
+  let newImg = document.createElement('img');
+  newImg.className = 'card-img-top';
+  newImg.src = hats.imageHref;
+  newImg.alt = 'Image of baseball cap';
+  newDiv2.appendChild(newImg);
+
+  // creates the the 4th <div> with class="card-body text-center"(nested within the 2nd <div> too)
+  div = document.getElementsByClassName('card my-3');
+  let newDiv4 = document.createElement('div');
+  newDiv4.className = 'card-body text-center';
+  newDiv2.appendChild(newDiv4);
+
+  // creates the title <h5> (nested within the 4th <div> too)
+  div = document.getElementsByClassName('card-body text-center')
+  let newTitle = document.createElement('h5');
+  newTitle.className = 'card-title';
+  newTitle.textContent = hats.name;
+  newDiv4.appendChild(newTitle);
+
+  // creates the paragraph <p>
+  div = document.getElementsByClassName('card-body text-center');
+  let newPe = document.createElement('p');
+  newPe.className = 'card-text';
+  newPe.textContent = 'Color: ';
+  let em = document.createElement('em');
+  em.textContent = hats.color;
+  newPe.appendChild(em);
+  newDiv4.appendChild(newPe);
+
+  // creates the button "Add to wishlist!"
+  div = document.getElementsByClassName('card-body text-center')
+  let newButton = document.createElement('button');
+  newButton.className = 'btn btn-outline-primary';
+  newButton.textContent = 'Add to wishlist!';
+  newDiv4.appendChild(newButton);
+}
+
+displayHat(hats);

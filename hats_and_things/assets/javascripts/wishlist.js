3c1fa86669a510b71e24a31af85352907be9b006
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-05-14 17:17:19 +0200

    WishList task five
    creates the displayWishlist() function

--- hats_and_things/assets/javascripts/wishlist.js
+++ hats_and_things/assets/javascripts/wishlist.js
@@ -14,3 +14,77 @@ function getAccessories() {
 }
 
 let storedItems = getAccessories();
+
+// Function that create an Accessory object
+function displayAccessory(accessory) {
+    // creates the 1st <div> with class="col-sm-4"
+    let parentProducts = document.querySelector('#products'); // (this is the container)
+    let accessoryComponent = document.createElement('div');
+    accessoryComponent.className = 'accessory col-sm-4 ' + accessory.color;
+    parentProducts.appendChild(accessoryComponent);
+
+    // creates the 2nd <div> with class="card my-3" (nested within the 1st <div>)
+    let prevComponent = document.getElementsByClassName('accessory col-sm-4 ');
+    let accessoryCard = document.createElement('div');
+    accessoryCard.className = 'card my-3';
+    accessoryComponent.appendChild(accessoryCard);
+    // console.log(prevComponent);
+
+    // creates the 3rd <div> with class="currency btn btn-light disabled" (nested within the 2nd <div>)
+    prevComponent = document.getElementsByClassName('card my-3');
+    let accessoryCurrency = document.createElement('div');
+    accessoryCurrency.className = 'currency btn btn-light disabled';
+    accessoryCurrency.textContent = accessory.price;
+    accessoryCard.appendChild(accessoryCurrency);
+
+    // creates the <img> (nested also within the 2nd <div>)
+    prevComponent = document.getElementsByClassName('currency btn btn-light disabled');
+    let accessoryImage = document.createElement('img');
+    accessoryImage.className = 'card-img-top';
+    accessoryImage.src = accessory.imageHref;
+    accessoryImage.alt = "Image of " + accessory.name;
+    accessoryCard.appendChild(accessoryImage);
+
+    // creates the the 4th <div> with class="card-body text-center"(nested within the 2nd <div> too)
+    prevComponent = document.getElementsByClassName('card my-3');
+    let accessoryBody = document.createElement('div');
+    accessoryBody.className = 'card-body text-center';
+    accessoryCard.appendChild(accessoryBody);
+
+    // creates the title <h5> (nested within the 4th <div> too)
+    prevComponent = document.getElementsByClassName('card-body text-center');
+    let accessoryTitle = document.createElement('h5');
+    accessoryTitle.className = 'card-title';
+    accessoryTitle.textContent = accessory.name;
+    accessoryBody.appendChild(accessoryTitle);
+
+    // creates the paragraph <p>
+    prevComponent = document.getElementsByClassName('card-body text-center');
+    let accessoryText = document.createElement('p');
+    accessoryText.className = 'card-text';
+    accessoryText.textContent = 'Color: ';
+    let em = document.createElement('em');
+    em.textContent = accessory.color;
+    accessoryText.appendChild(em);
+    accessoryBody.appendChild(accessoryText);
+}
+
+function displayAccessoryBtnRemove() {
+    // creates the "Remove" button
+    prevComponent = document.getElementsByClassName('card-body text-center');
+    accessoryBody = prevComponent[prevComponent.length - 1];
+    let accessoryButton = document.createElement('button');
+    accessoryButton.className = 'btn btn-outline-danger';
+    accessoryButton.textContent = 'Remove';
+    accessoryBody.appendChild(accessoryButton);
+}
+
+// creates the displayWishlist() function
+function displayWishlist() {
+    for (let i = 0; i < storedItems.length; i++) {
+        displayAccessory(storedItems[i])
+        displayAccessoryBtnRemove(storedItems[i])
+    }
+}
+
+displayWishlist();
